a4d340edc344920b0145848fb5debc0eb6ab2aba
GourishPadihar <63400249+GourishPadihar@users.noreply.github.com>
2020-12-08 13:39:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from "react";
-import Province from "./province";
+import Province from "./Province";
 import "./../styles/App.css";
 
 // Do not alter the states const and values inside it.
